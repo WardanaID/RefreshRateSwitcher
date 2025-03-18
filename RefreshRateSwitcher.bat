d258779ecb72fee9360f6fc124c5307e8844b840
WardanaID <47153051+WardanaID@users.noreply.github.com>
2025-03-18 13:09:03 +0700

    Update RefreshRateSwitcher.bat

--- RefreshRateSwitcher.bat
+++ RefreshRateSwitcher.bat
@@ -1,4 +1,2 @@
 @echo off
-echo %DATE% %TIME% - BAT File Running >> refresh_log.txt
-powershell -ExecutionPolicy Bypass -NoProfile -File "%~dp0RefreshRateSwitcher.ps1"
-echo %DATE% %TIME% - Script Executed >> refresh_log.txt
+powershell -ExecutionPolicy Bypass -File "D:\[DIREKTORI RRS]\RefreshRateSwitcher.ps1"
